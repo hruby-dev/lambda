006de1d4cd4a1e3970d2a6bfdd934109c3066527
frantisek.hruby <frantisek@hruby.xyz>
2025-11-17 20:23:23 +0100

    templates

--- templates/rev_start.sh
+++ templates/rev_start.sh
@@ -4,7 +4,6 @@ set -e
 CHALL_NAME="jumping-jacks"
 
 mkdir -p /handout/$CHALL_NAME
-cp /opt/main.c /handout/$CHALL_NAME/
 cp /opt/chall /handout/$CHALL_NAME/
 
 socat -T30 TCP-LISTEN:4444,reuseaddr,fork EXEC:/opt/chall
